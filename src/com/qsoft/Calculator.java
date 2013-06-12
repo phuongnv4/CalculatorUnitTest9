cd3dc406a75b1969284c545e879942d7d9a0947f
phuongnv4 <phuongk48a1t@gmail.com>
2013-06-12 15:55:38 +0700

    addNumberNewLine green

--- src/com/qsoft/Calculator.java
+++ src/com/qsoft/Calculator.java
@@ -9,7 +9,24 @@ public class Calculator {
 		if (str.isEmpty()) {
 			return 0;
 		}
-		return 1;
+		str = str.replace("\n", "");
+		String[] strTemp = str.split(",");
+		return sum(strTemp);
 	}
 
+	public static int sum(String[] str) {
+		int sum = 0;
+		int strTempSize = str.length;
+		for (int i = 0; i < strTempSize; i++) {
+			String str1 = str[i];
+			if (str1.isEmpty()) {
+				continue;
+			}
+			if (Integer.parseInt(str1) < 0) {
+				throw new RuntimeException("negatives not allowed");
+			}
+			sum += Integer.parseInt(str1);
+		}
+		return sum;
+	}
 }
